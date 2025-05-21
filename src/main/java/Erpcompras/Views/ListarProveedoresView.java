53bff08fd283535d2d2890ad29306355423647c7
Ivanna Nievecela <ivannanievecela@gmail.com>
2025-05-21 16:33:35 -0500

    FIX(UPS-003): Funcionamiento de botones

--- src/main/java/Erpcompras/Views/ListarProveedoresView.java
+++ src/main/java/Erpcompras/Views/ListarProveedoresView.java
@@ -5,24 +5,19 @@ import java.awt.event.*;
 
 public class ListarProveedoresView extends Frame {
     public ListarProveedoresView() {
+        //Propiedades de ventana
         setTitle("Lista de Proveedores");
         setSize(400, 300);
         setLayout(new BorderLayout());
 
+        //Mostrara la lista de proveedores
         TextArea area = new TextArea();
         area.setText("Aquí iría la lista de proveedores...");
 
-        Button volverBtn = new Button("Volver");
-
+        //Componentes de la ventana
         add(area, BorderLayout.CENTER);
-        add(volverBtn, BorderLayout.SOUTH);
 
-        volverBtn.addActionListener(e -> {
-            dispose();
-            new MenuPrincipalView().setVisible(true);
-        });
-
-
+        // Manejo de cierre
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 dispose();
