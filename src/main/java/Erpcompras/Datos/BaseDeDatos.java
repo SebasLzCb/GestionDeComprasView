7cc7b59a706dd272c92084e2a0253fd8cd2bed77
Ivanna Nievecela <ivannanievecela@gmail.com>
2025-05-24 21:00:49 -0500

    FIX(UPS-006): Implementación de la carpeta DATOS y creación de la clase BaseDeDatos.

---
+++ src/main/java/Erpcompras/Datos/BaseDeDatos.java
@@ -0,0 +1,10 @@
+package Erpcompras.Datos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseDeDatos {
+    public static List<String> productos = new ArrayList<>();
+    public static List<String> proveedores = new ArrayList<>();
+    public static List<String> solicitudes = new ArrayList<>();
+}
